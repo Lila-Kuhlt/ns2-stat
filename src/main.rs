bdc62931a9da2a3417c195ba07d88e177a1a19c9
Aaron Geiger <aaron@geigr.io>
2022-01-15 04:05:56 +0100

    Rename input.json -> test_data.json

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod types;
 
 fn main() {
-    let input = include_str!("input.json");
+    let input = include_str!("test_data.json");
     let parse: types::NS2Stats = serde_json::from_str(input).unwrap();
 
     println!("{parse:?}");
