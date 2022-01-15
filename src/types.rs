4a75705afa8b3bc78ba0944fc815d65014879f09
Dennis <dennis@kobert.dev>
2022-01-15 05:20:53 +0100

    Update struct defenitions

--- src/types.rs
+++ src/types.rs
@@ -31,18 +31,18 @@ impl GameStats {
 pub struct Building {
     #[serde(rename = "teamNumber")]
     pub team_number: i64,
-    #[serde(rename = "techId")]
-    pub tech_id: TechId,
-    pub built: bool,
-    #[serde(rename = "entityId")]
-    pub entity_id: i64,
-    pub event: Event,
-    pub location: String,
     #[serde(rename = "gameTime")]
     pub game_time: f64,
-    pub destroyed: bool,
+    pub built: bool,
+    pub location: String,
     pub recycled: bool,
+    pub destroyed: bool,
+    #[serde(rename = "techId")]
+    pub tech_id: TechId,
     pub biomass: Option<i64>,
+    #[serde(rename = "entityId")]
+    pub entity_id: Option<i64>,
+    pub event: Option<Event>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -50,29 +50,29 @@ pub struct KillFeed {
     #[serde(rename = "killerWeapon")]
     pub killer_weapon: KillerWeapon,
     #[serde(rename = "killerSteamID")]
-    pub killer_steam_id: i64,
+    pub killer_steam_id: Option<i64>,
+    #[serde(rename = "doerPosition")]
+    pub doer_position: Option<String>,
     #[serde(rename = "killerLocation")]
-    pub killer_location: i64,
+    pub killer_location: Option<i64>,
     #[serde(rename = "killerPosition")]
-    pub killer_position: String,
+    pub killer_position: Option<String>,
     #[serde(rename = "killerClass")]
-    pub killer_class: KillerClass,
+    pub killer_class: Option<VictimClass>,
+    #[serde(rename = "doerLocation")]
+    pub doer_location: Option<i64>,
     #[serde(rename = "killerTeamNumber")]
     pub killer_team_number: i64,
     #[serde(rename = "victimLocation")]
-    pub victim_location: i64,
+    pub victim_location: Option<i64>,
     #[serde(rename = "victimSteamID")]
     pub victim_steam_id: i64,
     #[serde(rename = "victimClass")]
-    pub victim_class: KillerClass,
+    pub victim_class: VictimClass,
     #[serde(rename = "victimPosition")]
     pub victim_position: String,
     #[serde(rename = "gameTime")]
     pub game_time: f64,
-    #[serde(rename = "doerPosition")]
-    pub doer_position: Option<String>,
-    #[serde(rename = "doerLocation")]
-    pub doer_location: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -103,28 +103,28 @@ pub struct PlayerStat {
     pub the_1: HashMap<String, f64>,
     #[serde(rename = "2")]
     pub the_2: HashMap<String, f64>,
-    #[serde(rename = "commanderSkillOffset")]
-    pub commander_skill_offset: i64,
-    #[serde(rename = "commanderSkill")]
-    pub commander_skill: i64,
-    #[serde(rename = "playerName")]
-    pub player_name: String,
-    pub status: Vec<Status>,
     #[serde(rename = "isRookie")]
     pub is_rookie: bool,
-    #[serde(rename = "playerSkillOffset")]
-    pub player_skill_offset: i64,
+    pub weapons: Weapons,
+    pub status: Vec<Status>,
     #[serde(rename = "lastTeam")]
     pub last_team: i64,
     #[serde(rename = "hiveSkill")]
     pub hive_skill: i64,
-    pub weapons: HashMap<String, Weapon>,
+    #[serde(rename = "playerName")]
+    pub player_name: String,
+    #[serde(rename = "commanderSkillOffset")]
+    pub commander_skill_offset: Option<i64>,
+    #[serde(rename = "commanderSkill")]
+    pub commander_skill: Option<i64>,
+    #[serde(rename = "playerSkillOffset")]
+    pub player_skill_offset: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Status {
     #[serde(rename = "statusId")]
-    pub status_id: KillerClass,
+    pub status_id: VictimClass,
     #[serde(rename = "classTime")]
     pub class_time: f64,
 }
@@ -151,7 +151,7 @@ pub struct Research {
     #[serde(rename = "gameTime")]
     pub game_time: f64,
     #[serde(rename = "researchId")]
-    pub research_id: String,
+    pub research_id: ResearchId,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -191,15 +191,22 @@ pub struct ServerInfo {
     #[serde(rename = "buildNumber")]
     pub build_number: i64,
     pub ip: String,
-    pub name: String,
+    pub name: ServerInfoName,
     pub port: i64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Mod {
     #[serde(rename = "modId")]
-    pub mod_id: String,
-    pub name: String,
+    pub mod_id: ModId,
+    pub name: ModName,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Weapons {
+    AnythingArray(Vec<Option<serde_json::Value>>),
+    WeaponMap(HashMap<String, Weapon>),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -213,12 +220,14 @@ pub enum Event {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum TechId {
+    AdvancedArmory,
     #[serde(rename = "ARC")]
     Arc,
     #[serde(rename = "ARCRoboticsFactory")]
     ArcRoboticsFactory,
     Armory,
     ArmsLab,
+    BabblerEgg,
     CommandStation,
     Crag,
     CragHive,
@@ -243,6 +252,7 @@ pub enum TechId {
     ShadeHive,
     Shell,
     Shift,
+    ShiftHive,
     Spur,
     Tunnel,
     Veil,
@@ -250,26 +260,124 @@ pub enum TechId {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum KillerClass {
+pub enum VictimClass {
     Commander,
     Dead,
+    DeathTrigger,
     Embryo,
+    Exo,
+    Fade,
+    FadeEgg,
     Flamethrower,
     Gorge,
+    GorgeEgg,
+    GrenadeLauncher,
     HeavyMachineGun,
+    Lerk,
+    LerkEgg,
     Onos,
+    OnosEgg,
     Rifle,
+    Shotgun,
     Skulk,
+    Void,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum KillerWeapon {
+    Axe,
+    Babbler,
     Bite,
     Flamethrower,
     Gore,
+    GrenadeLauncher,
     HeavyMachineGun,
+    LayMines,
+    LerkBite,
+    Minigun,
+    None,
+    Parasite,
+    Pistol,
+    Railgun,
     Rifle,
     Sentry,
+    Shotgun,
+    Spikes,
+    Spit,
+    Spores,
+    Spray,
+    Stomp,
+    Swipe,
     Welder,
     Whip,
+    Xenocide,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ResearchId {
+    AdvancedArmoryUpgrade,
+    AdvancedMarineSupport,
+    AdvancedWeaponry,
+    Armor1,
+    Armor2,
+    Armor3,
+    BileBomb,
+    BoneShield,
+    Charge,
+    ExosuitTech,
+    FadeEgg,
+    GorgeEgg,
+    GrenadeTech,
+    JetpackTech,
+    Leap,
+    LerkEgg,
+    MetabolizeEnergy,
+    MetabolizeHealth,
+    MinesTech,
+    OnosEgg,
+    PhaseTech,
+    ResearchBioMassOne,
+    ResearchBioMassThree,
+    ResearchBioMassTwo,
+    ShotgunTech,
+    Spores,
+    Stab,
+    Stomp,
+    Umbra,
+    UpgradeRoboticsFactory,
+    UpgradeToCragHive,
+    UpgradeToInfestedTunnel,
+    UpgradeToShadeHive,
+    UpgradeToShiftHive,
+    Weapons1,
+    Weapons2,
+    Weapons3,
+    Xenocide,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ModId {
+    #[serde(rename = "4241b84e")]
+    The4241B84E,
+    #[serde(rename = "9e113555")]
+    The9E113555,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ModName {
+    #[serde(rename = "Matched Play Balance")]
+    MatchedPlayBalance,
+    #[serde(rename = "UWE Extension")]
+    UweExtension,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ServerInfoName {
+    Leid,
+    #[serde(rename = "Lila Pause")]
+    LilaPause,
+    #[serde(rename = "Lila Pause aber cool")]
+    LilaPauseAberCool,
+    #[serde(rename = "NS2 Dedicated Server")]
+    Ns2DedicatedServer,
 }
